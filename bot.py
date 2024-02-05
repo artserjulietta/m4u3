0af0ad36a93e5d12b8f9c19082736d0e417c6984
artserjulietta <110678368+artserjulietta@users.noreply.github.com>
2024-02-05 19:39:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,19 +22,27 @@ def addtask_command(message):
 
 def save_task(message):
     name = message.text
-    user_id = message.chat.id  
+    user_id = message.from_user.id 
     task_manager.add_task(user_id, name, '')
     bot.send_message(message.chat.id, "Задача добавлена")
 
 @bot.message_handler(commands=['delete_task'])
 def deletetask_command(message):
-    bot.send_message(message.chat.id, "Введите имя задачи, которую хотите удалить:")
+    bot.send_message(message.from_user.id, "Введите имя задачи, которую хотите удалить:")
     bot.register_next_step_handler(message, delete_task_by_id)
 
 def delete_task_by_id(message):
+    user_id = message.from_user.id  
     task_name = message.text
-    task_manager.delete_task(task_name)
+    task_manager.delete_task(task_name, user_id)
     bot.send_message(message.chat.id, "Задача удалена")
+
+@bot.message_handler(commands=['show'])
+def show(message):
+    user_id = message.from_user.id 
+    tasks = task_manager.show_task(user_id)
+    tasks =  "\n".join([x[0] for x in tasks])
+    bot.send_message(message.chat.id, tasks)
     
 
 bot.infinity_polling()
